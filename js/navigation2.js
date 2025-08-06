c2f879ffb258adc31f3e52edfe0c6ef26ba591e3
ClayAucoin <clay0aucoin@gmail.com>
2025-08-06 10:13:13 -0500

    .

--- js/navigation2.js
+++
@@ -1,6 +0,0 @@
-fetch('/weekly-reflections/navigation.html')
-    .then(response => response.text())
-    .then(html => {
-        document.getElementById('navigation2').innerHTML = html;
-    })
-    .catch(error => console.error('Error loading content:', error));
